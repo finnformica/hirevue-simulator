839e1a4b009220de8c24f9e4bdaa4735090d0396
finnformica <finnformica@gmail.com>
2025-05-26 09:52:14 +0100

    chore: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,23 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gradguru - Interview Preparation",
+  title: "Gradguru",
   description:
     "Gradguru is a platform designed to help students prepare for interviews with resources, practice questions, and community support.",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon.ico" },
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/favicon/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
