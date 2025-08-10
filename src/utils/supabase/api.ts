48d7109fed4de71f9d9cf8ffa67e9a3f5e9f825b
finnformica <finnformica@gmail.com>
2025-08-10 18:52:26 +0100

    update .vercelignore to be more finegrained

--- src/utils/supabase/api.ts
+++
@@ -1,7 +0,0 @@
-// IMPORTANT = remove this file in favour of the server side supabase client
-import { createClient } from "@supabase/supabase-js";
-
-export const supabaseApi = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
